75acac4b0f9f148f09432866fcae1fdfa7a13f08
Matt Clifford <mc15445@my.bristol.ac.uk>
2021-12-31 21:05:00 +0000

    more negation

--- tests/test_negation.py
+++ tests/test_negation.py
@@ -11,7 +11,35 @@ meta.initialise_prolexa(pl)
 def ask(input):
     return meta.standardised_query(pl, input)[0]['Output']
 
-def test_negation():
+def test_negation_explain():
     ask('matt is not happy')
     ans = ask('explain why matt is not a teacher')
     assert ans == 'matt is not happy ; every teacher is happy ; therefore matt is not a teacher'
+
+# the following works in ipython but not python...
+# def test_negation_who():
+#     ans = ask('who is not a teacher')
+#     assert ans == b'pixie is not a teacher'
+
+def test_deletion():
+    ask('matt is happy')      # one rule
+    ans = ask('is matt happy')
+    assert ans == b'matt is happy'
+
+    ask('matt is not happy')  # add the negation
+    ans = ask('is matt not happy')
+    assert ans == b'matt is not happy'
+
+    # the following works in swipl and ipython but not python ...
+    # ans = ask('is matt not happy')
+    # assert ans == b'matt is not happy'
+
+if __name__ == '__main__':
+    pl = Prolog()
+    meta.reset_grammar()
+    meta.initialise_prolexa(pl)
+    ask('matt is happy')
+    ans = ask('is matt happy')
+    ask('matt is not happy')
+    ans = ask('is matt happy')
+    print(ans) ## prints('matt is not happy') in ipython
